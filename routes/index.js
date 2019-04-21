aab96d5bb9b74b9ce27a8c5fdbc3c6fcffff9ef1
boosey <boosey.boudreaux@gmail.com>
2019-04-20 20:39:42 -0500

    Spillway

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World Again' });
+  res.render('index', { title: 'Spillway' });
 });
 
 module.exports = router;
